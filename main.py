48f9902357ecfc5dd1f5a0cd5432503de517e0a1
gleb165 <83755952+gleb165@users.noreply.github.com>
2023-11-28 13:53:04 +0200

    first step in fastapi

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+@app.get("/name/{name}")
+async def root(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {'model_name': 'alexnet', 'message': 'Deep Learning FTW!'}
+    if model_name.value == ModelName.resnet:
+        return {'model_name': 'resnet', 'message': 'LeCNN all the images'}
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return {'model_name': 'lenet', 'message': 'Have some residuals'}
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
