80d4436bb07c95d43295ea731a08b52e80c5f12a
gleb165 <83755952+gleb165@users.noreply.github.com>
2023-11-30 17:16:00 +0200

    3

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Annotated
-
-name = 'Pydantic PyCharm Plugin'
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
 
 app = FastAPI()
 
 
-@app.post('/items/{items}')
-async def first_post(items_id: int, item: Item, q: str | None = None):
-    if q:
-        return {'q': q}
-
-    return {'items_id': items_id, **item.dict()}
+class Item(BaseModel):
+    name: str
+    description: str
 
 
+items = [
+    {"name": "Foo", "description": "There comes my hero"},
+    {"name": "Red", "description": "It's my aeroplane"},
+]
 
-@app.get("/items/")
-async def read_items(q: Annotated[list[str] | None, Query()] = None):
-    query_items = {"q": q}
-    return query_items
+
+@app.get("/items/", response_model=list[Item])
+async def read_items():
+    return items
